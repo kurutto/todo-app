da405972afe50a74838da01f24a7a1206db86b67
kurutto <namihojo@gmail.com>
2025-02-15 08:08:35 +0900

    useFormを使用して作成

--- src/components/CredentialSignin.tsx
+++ src/components/CredentialSignin.tsx
@@ -1,31 +1,71 @@
-'use client'
-import { signIn } from 'next-auth/react';
-import React, { useRef } from 'react'
+"use client";
+import { signIn } from "next-auth/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Paragraph from "./ui/paragraph";
+import Block from "./ui/block";
+import { Label } from "@radix-ui/react-label";
+
+const formSchema = z.object({
+  id: z.string({
+    required_error: "必須項目です",
+    invalid_type_error: "入力値に誤りがります",
+  }),
+  password: z.string({
+    required_error: "必須項目です",
+    invalid_type_error: "入力値に誤りがります",
+  }),
+});
+type formType = z.infer<typeof formSchema>;
 
 const CredentialSignin = () => {
-  const idRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    await signIn("credentials", {
-      id:idRef.current!.value,
-      password:passwordRef.current!.value,
-      callbackUrl: '/todos'
+  const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm<formType>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        id: "",
+        password: "",
+      },
     });
-  }
+  const onSubmit = async (values: formType) => {
+    await signIn("credentials", {
+      id: values.id,
+      password: values.password,
+      callbackUrl: "/todos",
+    });
+  };
   return (
     <div>
-      <h2>ログイン</h2>
-      <form onSubmit={handleSubmit}>
-        <label>ID</label>
-        <input type='text' ref={idRef} />
-        <label>パスワード</label>
-        <input type='password' ref={passwordRef} />
-        <button type='submit'>ログイン</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {errors.root && (
+          <Paragraph variant="error">{errors.root.message}</Paragraph>
+        )}
+        <Block>
+          <Label htmlFor="id">ID</Label>
+          <Input type="text" id="id" {...register("id")} />
+          {errors.id && (
+            <Paragraph variant="error">{errors.id.message}</Paragraph>
+          )}
+        </Block>
+        <Block>
+          <Label htmlFor="password">パスワード</Label>
+          <Input type="password" id="password" {...register("password")} />
+          {errors.password && (
+            <Paragraph variant="error">{errors.password.message}</Paragraph>
+          )}
+        </Block> 
+        <Button type="submit" className="mt-10 w-48 block mx-auto">
+          送信
+        </Button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default CredentialSignin
+export default CredentialSignin;
