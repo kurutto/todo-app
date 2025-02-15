9d41d399212a6b8d561e87209910fd70068d7d2d
kurutto <namihojo@gmail.com>
2025-02-15 20:45:27 +0900

    class名を統一するため修正

--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import React, { ReactNode } from "react";
 
 interface headingProps
-  extends Omit<React.ComponentPropsWithoutRef<'h1'>, 'className'> {
+  extends Omit<React.ComponentPropsWithoutRef<"h1">, "className"> {
   level?: 1 | 2 | 3 | 4 | 5;
   children: ReactNode;
   className?: string;
@@ -14,13 +14,13 @@ const Heading = ({
   ...props
 }: headingProps) => {
   const Tag = `h${level}` as const;
-  const style = cn(
+  const baseStyle = cn(
     level === 1 && "text-2xl text-center font-semibold mb-12",
     level === 2 && "text-lg text-center mb-6 mt-12"
   );
   return (
     <>
-      <Tag className={cn(style, className)} {...props}>
+      <Tag className={cn(baseStyle, className)} {...props}>
         {children}
       </Tag>
     </>
