35c681426a67c75dfd532e064c4bb4efaf12b9f0
kurutto <namihojo@gmail.com>
2025-02-15 10:02:28 +0900

    todo作成フォームをuseFormで作成

--- src/components/CreateTodo.tsx
+++ src/components/CreateTodo.tsx
@@ -1,7 +1,41 @@
 'use client'
 import { useRouter } from 'next/navigation';
-import React, { useRef } from 'react'
+import React, { useRef } from 'react';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Paragraph from "./ui/paragraph";
+import Block from "./ui/block";
+import { Label } from "@radix-ui/react-label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
+
+import { statusList } from '@/data/todos/status';
+import { Textarea } from './ui/textarea';
+
+const formSchema = z.object({
+  title: z
+    .string()
+    .min(1, {
+      message: "必須項目です",
+    }),
+  content: z
+    .string()
+    .max(50, {
+      message: "50文字以内で入力してください",
+    }),
+  status: z
+    .string()
+});
+type formType = z.infer<typeof formSchema>;
 interface CreateTodoProps {
   userId:string;
 }
@@ -11,24 +45,61 @@ const CreateTodo = ({userId}:CreateTodoProps) => {
   const titleRef = useRef<HTMLInputElement>(null);
   const contentRef = useRef<HTMLTextAreaElement>(null);
   const statusRef = useRef<HTMLSelectElement>(null);
-  const statusList = ['未完了','途中','完了'];
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    await fetch(`${process.env.NEXT_PUBLIC_API_URL}/todos/${userId}`, {
-      method: "POST",
-      body: JSON.stringify({
-        title: titleRef.current!.value ? titleRef.current!.value : "",
-        content: contentRef.current!.value ? contentRef.current!.value : "",
-        status: Number(statusRef.current!.value)
-      }),
+  const {
+      register,
+      handleSubmit,
+      formState: { errors },
+      setError,
+    } = useForm<formType>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        title: "",
+        content: "",
+        status: '1',
+      },
     });
+    const onSubmit = async (values: formType) => {
+      await fetch(`${process.env.NEXT_PUBLIC_API_URL}/todos/${userId}`, {
+        method: "POST",
+        body: JSON.stringify({
+          title: values.title,
+          content: values.content,
+          status: Number(values.status)
+        }),
+      });
     router.push("/todos");
     router.refresh();
   };
   return (
-    
-    <form onSubmit={handleSubmit}>
-    <label>タイトル</label>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Block>
+        <Label htmlFor="title">タイトル</Label>
+        <Input type="text" id="id" {...register("title")} />
+        {errors.title && (
+          <Paragraph variant="error">{errors.title.message}</Paragraph>
+        )}
+      </Block>
+      <Block>
+        <Label htmlFor="content">内容</Label>
+        <Textarea id="content" {...register("content")} />
+        {errors.content && (
+          <Paragraph variant="error">{errors.content.message}</Paragraph>
+        )}
+      </Block>
+      <Block>
+        <Label htmlFor="status">ステータス</Label>
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="ステータスを選択" />
+          </SelectTrigger>
+          <SelectContent id="content" {...register("status")} >
+            {statusList.map((status,idx) => (
+                <SelectItem key={idx} value={idx.toString()}>{status}</SelectItem>
+              ))}
+          </SelectContent>
+        </Select>
+      </Block>
+    {/* <label>タイトル</label>
     <div>
       <input type="text" ref={titleRef} />
     </div>
@@ -38,13 +109,17 @@ const CreateTodo = ({userId}:CreateTodoProps) => {
     </div>
     <label>ステータス</label>
     <div>
+    
       <select ref={statusRef}>
         {statusList.map((status,idx) => (
           <option key={idx} value={idx}>{status}</option>
         ))}
       </select>
-    </div>
-    <button type="submit">送信</button>
+    </div> */}
+    <Button type="submit" className="mt-10 w-48 block mx-auto">
+      送信
+    </Button>
+    {/* <button type="submit">送信</button> */}
   </form>
   )
 }
