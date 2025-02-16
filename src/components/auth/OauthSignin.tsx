2c4605348537de7fcf0d7e73a072a9f34a3ca028
kurutto <namihojo@gmail.com>
2025-02-16 21:57:44 +0900

    OAuthアカウントの選択肢が将来的にも多くならないと予想されるので、手動でのアカウントボタン追加に変更

--- src/components/auth/OauthSignin.tsx
+++ src/components/auth/OauthSignin.tsx
@@ -1,41 +1,18 @@
 "use client";
-import { ClientSafeProvider, getProviders } from "next-auth/react";
-import React, { ReactNode, useEffect, useState } from "react";
+import { FaGoogle } from "react-icons/fa";
 import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 
-interface OauthSigninProps {
-  className ?: string;
-}
-const OauthSignin = ({className}:OauthSigninProps) => {
-  const [providers, setProviders] = useState<{
-    [key: string]: ClientSafeProvider;
-  } | null>(null);
-
-  useEffect(() => {
-    const fetchProviders = async () => {
-      const res = await getProviders();
-      setProviders(res);
-    };
-    fetchProviders();
-  }, []);
+const OauthSignin = () => {
   return (
     <div>
-      {providers &&
-        Object.values(providers).map((provider) =>
-          provider.id != "credentials" && provider.id != "email" ? (
-            <Button
-              key={provider.id}
-              onClick={() => signIn(provider.id, { callbackUrl: "/todos" })}
-              className={cn ( provider.id==='google' && 'bg-[#dd5144] hover:bg-[#dd5144] hover:opacity-90','w-full')}
-            >
-              {provider.name}
-            </Button>
-          ) : (
-            ""
-          )
-        )}
+      <Button
+        onClick={() => signIn('google', { callbackUrl: "/todos" })}
+        variant="outline"
+        className="w-full"
+      ><FaGoogle />
+        Googleアカウントから
+      </Button>
     </div>
   );
 };
