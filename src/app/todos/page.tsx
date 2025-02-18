85ad8ae293619e7416e9061df30297aad8389143
kurutto <namihojo@gmail.com>
2025-02-18 18:02:46 +0900

    スタイルの調整

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -15,11 +15,11 @@ const page = async () => {
   const todos = await fetchTodos(user.id);
   return (
     <div>
-      <Heading level={1}>TODO一覧</Heading>
+      <div className="relative">
+        <Heading level={1}>TODO一覧</Heading>
+        <div className="sm:absolute sm:top-0 sm:right-0 max-sm:text-right max-sm:-mt-6 max-sm:mb-6"><Button asChild variant="secondary"><Link href="/todos/create">新規作成</Link></Button></div>
+      </div>
       <Todos todos={todos} />
-      <Block margin="lg" className="text-center">
-        <Button asChild variant="secondary"><Link href="/todos/create">新規作成</Link></Button>
-      </Block>
     </div>
   );
 };
