2fcaa83b1ac237e00f3e10ff0c7d954d142e2a6e
kurutto <namihojo@gmail.com>
2025-02-18 14:14:28 +0900

    ヘッダーのナビゲーションを設定

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,15 +3,22 @@ import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { UserType } from "@/types/types";
 import { nextAuthOptions } from "@/lib/next-auth/route";
+import Image from "next/image";
+import { FaUserCircle } from 'react-icons/fa';
+import { Button } from "../ui/button";
 
 const Header = async () => {
   const session = await getServerSession(nextAuthOptions);
   const user = session?.user as UserType;
+  console.log(user);
   return (
-    <div>
-      <Link href={user ? "/api/auth/signout" : "/signin"}>
+    <div className="flex items-center px-6 py-5 justify-end text-sm">
+      {!user && <Button variant="link"><Link href='/signup'>サインアップ</Link></Button>}
+      <Button variant="link"><Link href={user ? "/api/auth/signout" : "/signin"}>
         {user ? "ログアウト" : "ログイン"}
-      </Link>
+      </Link></Button>
+      {user && <><Button variant="link"><Link href='/todos'>TODO一覧</Link></Button><Link href='/mypage' className="ml-5">{user.image ? <Image src={user.image} alt="ユーザー情報" width='35' height='35' className="rounded-full" /> : <FaUserCircle className="ml-5" style={{ fontSize: '35px', color: 'gray' }} />}</Link></>}
+      
     </div>
   );
 };
