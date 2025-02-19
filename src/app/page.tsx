64a17121b0ae0c5d4b45ccd233ae62c72cc3705d
kurutto <namihojo@gmail.com>
2025-02-19 11:53:08 +0900

    トップページのリダイレクト作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { getServerSession } from "next-auth";
+import { nextAuthOptions } from "@/lib/next-auth/route";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(nextAuthOptions);
+  if (!session) {
+    redirect("/signin");
+  }else{
+    redirect("/todos")
+  }
+  
   return (
     <div>page</div>
   );
